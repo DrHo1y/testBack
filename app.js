9a7a7fa7ef71f30ba977f2968727ad26bc0e0289
Rymskiy Valentine <dr.holyblack@gmail.com>
2020-10-07 02:09:21 +0300

    add Console log

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  console.log('root')
+  console.log(req.method)
 })
 app.get('/lol', (req, res) => {
   res.send('Hey bro, Nice Dick')
+  console.log(req.method)
 })
 
 app.listen(port, () => {
+  console.clear()
   console.log(`Example app listening at http://localhost:${port}`)
 })
