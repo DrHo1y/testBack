7c45bd1374deec857bf35540a50f32957beca2d8
Rymskiy Valentine <dr.holyblack@gmail.com>
2020-10-09 03:02:17 +0300

    stop dev

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = 3000
+
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log('root')
-  console.log(req.method)
-})
-app.get('/lol', (req, res) => {
-  res.send('Hey bro, Nice Dick')
-  console.log(req.method)
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-app.listen(port, () => {
-  console.clear()
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://drholy:KteUGBjmZNx2Rvp@cluster0.d8rkl.mongodb.net/todos',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    )
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
